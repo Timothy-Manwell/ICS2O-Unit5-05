0605f71a9511776b269863053e5be88a19ffcbe5
Timothy-Manwell <Timothy-Manwell@users.noreply.github.com>
2022-12-01 19:44:20 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -21,31 +21,44 @@ function myButtonClicked() {
   const lengthC = parseFloat(document.getElementById("length-c").value)
 
   // using the cosine law
-  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
-  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
-  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+  const angleA =
+    Math.acos(
+      (lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)
+    ) *
+    (180 / Math.PI)
+  const angleB =
+    Math.acos(
+      (lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)
+    ) *
+    (180 / Math.PI)
+  const angleC =
+    Math.acos(
+      (lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)
+    ) *
+    (180 / Math.PI)
 
-  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  const sumOfAngles =
+    Number(angleA.toFixed(2)) +
+    Number(angleB.toFixed(2)) +
+    Number(angleC.toFixed(2))
 
   console.log(angleA)
   console.log(angleB)
   console.log(angleC)
   console.log(sumOfAngles)
 
-  if (sumOfAngles == 180)
-   {
+  if (sumOfAngles == 180) {
     if (lengthA == lengthC || lengthC == lengthB || lengthA == lengthB) {
       document.getElementById("answer").innerHTML =
-      "Your triangle is an isosceles triangle."
+        "Your triangle is an isosceles triangle."
     } else if (lengthA == lengthC && lengthA == lengthB && lengthB == lengthC) {
       document.getElementById("answer").innerHTML =
-      "Your triangle is an equilateral triangle."
+        "Your triangle is an equilateral triangle."
     } else if (lengthA != lengthC && lengthA != lengthB && lengthB != lengthC) {
       document.getElementById("answer").innerHTML =
-      "Your triangle is a scalene triangle."
+        "Your triangle is a scalene triangle."
     }
-   } else {
-    document.getElementById("answer").innerHTML =
-      "Error."
-   }
+  } else {
+    document.getElementById("answer").innerHTML = "Error."
+  }
 }
