eff85c47751f5ec34e9390c222839d1bec82fa78
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-01 14:48:23 -0500

    test

--- js/script.js
+++ js/script.js
@@ -47,13 +47,14 @@ function myButtonClicked() {
   console.log(angleC)
   console.log(sumOfAngles)
 
-  if (sumOfAngles == 180) {
-    if (lengthA == lengthC || lengthC == lengthB || lengthA == lengthB) {
+  if (sumOfAngles == 180)
+   {
+    if (lengthA == lengthC && lengthA == lengthB && lengthB == lengthC){
       document.getElementById("answer").innerHTML =
-        "Your triangle is an isosceles triangle."
-    } else if (lengthA == lengthC && lengthA == lengthB && lengthB == lengthC) {
+      "Your triangle is an equilateral triangle."
+    } else if (lengthA == lengthC || lengthC == lengthB || lengthA == lengthB) {
       document.getElementById("answer").innerHTML =
-        "Your triangle is an equilateral triangle."
+      "Your triangle is an isosceles triangle."
     } else if (lengthA != lengthC && lengthA != lengthB && lengthB != lengthC) {
       document.getElementById("answer").innerHTML =
         "Your triangle is a scalene triangle."
